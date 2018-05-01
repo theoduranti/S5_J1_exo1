a003e11eadb5928d097778712aab45f6df2d6299
Théo Duranti <theoduranti@hotmail.fr>
2018-05-01 18:23:17 +0200

    jusque la question 4 comprise

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Movie.destroy_all
+
+Movie.create(title:"Arrival",release_year:2016,director:"Denis Villeneuve")
+Movie.create(title:"Fight Club",release_year:1999,director:"David Fincher")
+Movie.create(title:"Requiem for a Dream",release_year:2001,director:"Darren Aronovsky")
+Movie.create(title:"Inception",release_year:2010,director:"Christopher Nolan")
+Movie.create(title:"Into the Wild",release_year:2007,director:"Sean Penn")
